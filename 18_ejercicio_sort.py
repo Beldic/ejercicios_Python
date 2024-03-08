75f7a9670cf723b1cce9033c0fbfb129b4ce5f30
Beldic <jordi_casado@hotmail.com>
2024-03-08 08:41:20 +0100

    Ejercicio 18 modificado

--- 18_ejercicio_sort.py
+++ 18_ejercicio_sort.py
@@ -1,5 +1,21 @@
+# Ejercicio 18 - Programa que ordena edades (listas)
+# Autor: Jordi Casado
+# Versión 1.0
+"""
 
+Crea un programa que le solicite al usuario que piense en 4 familiares cercanos, y que introduzca las edades
+de sus 4 familiares en 4 entradas por teclado diferentes que se almacenarán en una lista. 
+Posteriormente devolveremos la información de salida con el formato correpondiente:
 
+1. Inicializar las siguientes variables y asignar un valor de entrada por teclado: edad1, edad2, edad3 y edad4.
+
+2. Ordenar las edades de forma ascendente y descendente utilizando el método sort(). Y también el método sorted().
+
+3. Mostrar el resultado por salida utilizando el método print().
+
+"""
+
+# Yo le he hecho con append en vez de usar el método extend, ahorrando crear las cuatro variables.  ;)
 
 
 lista_familiares = []
